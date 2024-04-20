9774216e989b874728699cf1fc7617d3bd84e200
vijay vekariya <vijayvekariya007@gmail.com>
2024-04-20 16:56:20 +0530

    20-4-24

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_todo_app/auth/create_acc.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -18,19 +17,13 @@ class MyApp extends StatelessWidget {
     return Builder(
       builder: (BuildContext context) {
         // Get MediaQuery data
-        final MediaQueryData mediaQuery = MediaQuery.of(context);
+       
 
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'ToDo App',
           home: CreateAcc(),
           // Pass MediaQuery data down to your app
-          builder: (BuildContext context, Widget? child) {
-            return MediaQuery(
-              data: mediaQuery,
-              child: child!,
-            );
-          },
         );
       },
     );
